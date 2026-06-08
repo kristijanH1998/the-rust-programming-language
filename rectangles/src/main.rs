4a82b9f94cf5fdd0e65c07562902543349a13411
Kristijan Hornung <kristijan.hornung1@gmail.com>
2026-06-07 18:39:46 -0700

    structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -54,18 +54,64 @@
 
 // dbg! macro:
 
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let scale = 2;
+//     let rect1 = Rectangle {
+//         width: dbg!(30 * scale),
+//         height: 50,
+//     };
+
+//     dbg!(&rect1);
+// }
+
+// Methods:
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
+    let rect3 = Rectangle {
+        width: 40,
+        height: 15,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Rectangle 1 can hold Rectangle 3? {}", rect1.can_hold(&rect3));
+    let square1 = Rectangle::square(10);
+    println!("Square 1 has each side of length: {}", square1.width);
 }
