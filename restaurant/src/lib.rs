7684bfbbf1431c6de8b23c8750a7444dfb37938d
Kristijan Hornung <kristijan.hornung1@gmail.com>
2026-06-09 16:01:25 -0700

    modules in crates of restauran project reordered and separated

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,15 +1,49 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn seat_at_table() {}
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
     }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+// module 'hosting' brought into scope with 'use' keyword
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    // possible because of 'use crate::front_of_house::hosting;' keyword above
+    hosting::add_to_waitlist();
 }
