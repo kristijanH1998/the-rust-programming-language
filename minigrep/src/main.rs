527e8be18519dce11c1aac085ba472005f0619f4
Kristijan Hornung <kristijan.hornung1@gmail.com>
2026-06-15 21:22:29 -0700

    closures

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     // let file_path = &args[2];
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -20,7 +20,7 @@ fn main() {
 
     // let _ = run(config);
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
