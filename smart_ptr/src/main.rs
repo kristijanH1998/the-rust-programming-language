938c917cccce48c7e455509832533cb7ea500027
Kristijan Hornung <kristijan.hornung1@gmail.com>
2026-06-17 19:16:01 -0700

    reference counting pointer Rc<T>

--- smart_ptr/src/main.rs
+++ smart_ptr/src/main.rs
@@ -1,4 +1,6 @@
 use std::ops::Deref;
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 struct MyBox<T>(T);
 
@@ -26,6 +28,11 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
@@ -46,6 +53,16 @@ fn main() {
     println!("CustomSmartPointers created");
     drop(c);
     println!("CustomSmartPointer dropped before the end of main");
+
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
 
 fn hello(name: &str) {
