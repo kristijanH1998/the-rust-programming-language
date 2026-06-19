c2a52d7d2330571d7d2996c7a309132c31cf64b1
Kristijan Hornung <kristijan.hornung1@gmail.com>
2026-06-18 18:25:07 -0700

    threads

---
+++ threads/src/main.rs
@@ -0,0 +1,22 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        // for i in 1..10 {
+        //     println!("hi number {i} from the spawned thread!");
+        //     thread::sleep(Duration::from_millis(1));
+        // }
+
+        println!("Here's a vector: {v:?}");
+    });
+
+    // for i in 1..5 {
+    //     println!("hi number {i} from the main thread!");
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    handle.join().unwrap();
+}
